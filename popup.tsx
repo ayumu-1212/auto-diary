5bdd989a6175cdfacb91e497d98220cdb4aee2f4
ayumu-1212 <ayumuabe1434@gmail.com>
2023-07-19 01:23:53 +0900

    ✨ commit履歴を表示できるように

--- popup.tsx
+++ popup.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 function IndexPopup() {
   const [historyItems, setHistoryItems] = useState<chrome.history.HistoryItem[]>([])
+  const [commitItems, setCommitItems] = useState([])
 
   const getHistory = async() => {
     const now = new Date()
@@ -17,6 +18,29 @@ function IndexPopup() {
       maxResults,
     })
     setHistoryItems(items)
+  }
+
+  const getCommits = async() => {
+    // すぐ消す
+    const token="token"
+
+    const maxResults = 5
+    const items = []
+
+    await fetch(`https://api.github.com/repos/ayumu-1212/dnn_of_crystal_grain_diameter_distributional/commits?per_page=${maxResults}`, {
+      headers: {
+        Authorization: `token ${token}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(d => {
+        items.push(d.commit.message)
+      })
+    })
+    .catch(error => console.error(error))
+
+    setCommitItems(items)
   }
 
   return (
@@ -35,9 +59,15 @@ function IndexPopup() {
         Extension!
       </h2>
       <button id="getHistory" onClick={getHistory}>getHistory</button>
+      <button id="getCommits" onClick={getCommits}>getCommits</button>
       <div>
         <ul>
           {historyItems.map((item) => (<li id={item.id} key={item.id}>{item.title}</li>))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {commitItems.map((item) => (<li id={item.id} key={item.id}>{item}</li>))}
         </ul>
       </div>
     </div>
