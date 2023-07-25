e4ebc9d2776c1c76fc63a7406f51f5736eebd34d
ayumu-1212 <ayumuabe1434@gmail.com>
2023-07-26 01:21:52 +0900

    ✨ ほぼ完成

--- popup.tsx
+++ popup.tsx
@@ -1,71 +1,158 @@
-import { useState } from "react"
-import { getCommits } from "~apis/getCommits"
-import { getRepos } from "~apis/getRepos"
-import type { Repo } from "~apis/getRepos"
+import { useState, useEffect } from "react"
+import styled from "@emotion/styled"
 
-import { Flex, Heading, Spacer, Grid, GridItem } from '@chakra-ui/react'
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Grid } from '@mui/material'
+
 import { getDiaries } from "~hooks/getDiaries"
+import type { CommitDay } from "~hooks/getCommitDays"
+
+type HistoryDay = {
+  date: Date
+  items: chrome.history.HistoryItem[]
+}
 
 function IndexPopup() {
-  const [historyItems, setHistoryItems] = useState<chrome.history.HistoryItem[]>([])
-  const [commitItems, setCommitItems] = useState([])
-  const [repoItems, setRepoItems] = useState<Repo[]>([])
+  const [historyItems, setHistoryItems] = useState<HistoryDay[]>([])
 
-  const getHistory = async() => {
-    const now = new Date()
+  const [diaries, setDiaries] = useState<CommitDay[]>([])
 
-    const text = ""
-    const startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 0, 0, 0).getTime()
-    const endTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0).getTime()
-    const maxResults = 10
-    const items = await chrome.history.search({
-      text,
-      startTime,
-      endTime,
-      maxResults,
-    })
-    setHistoryItems(items)
-  }
+  const [headers, setHeaders] = useState<String[]>([])
 
-  const onRepos = async() => {
-    const repos = await getRepos()
-    setRepoItems(repos)
-  }
+  useEffect(() => {
+    const fetchCommitData = async() => {
+      const data = await getDiaries()
+      await setDiaries(data)
+      // await fetchSearchData()
+    }
+    fetchCommitData()
+  }, [])
 
-  const onCommits = async() => {
-    const commits = await getCommits({owner: 'ayumu-1212', repo: 'dnn_of_crystal_grain_diameter_distributional'})
-    setCommitItems(commits)
-  }
+  useEffect(() => {
+    const fetchSearchData = async() => {
+      const text = ""
+      const maxResults = 10
+      const historyItems: HistoryDay[] = []
+      for (const date of diaries.map((diary) => (diary.date))) {
+        const historyDay : HistoryDay = {
+          date: date,
+          items: []
+        }
+        const startTime = date.getTime()
+        const endTime = date.setHours(23)
+        const history = await chrome.history.search({
+          text,
+          startTime,
+          endTime,
+          maxResults,
+        })
+        historyDay.items = history
+        historyItems.push(historyDay)
+      }
+      setHistoryItems(historyItems)
+    }
+    fetchSearchData()
+  }, [diaries])
 
-  const onDiaries = async () => {
-    const data = await getDiaries()
-    console.log(data)
+  useEffect(() => {
+    const AI_TOKEN = process.env.PLASMO_PUBLIC_OPEN_AI_TOKEN
+    const headers = []
+    const onPostAi = async() => {
+      for (const index of [0, 1, 2, 3, 4]) {
+        const prompt = `以下はとあるエンジニアが同日で行った、Githubのコミットメッセージの履歴とGoogleの検索履歴である。この情報からエンジニアが1日で何をしたのかを50字で答えてください。\n\n
+          commitメッセージ\n
+          ${diaries.length > index ? diaries[index].commits.map((commit) => (`- ${commit.message}\n`)) : ''}\n
+          Googleの検索履歴\n
+          ${historyItems.length > index ? historyItems[index].items.map((item) => (`- ${item}\n`)) : ''}
+        `
+        const requestBody = {
+          model: "text-davinci-003",
+          prompt: prompt,
+          temperature: 1,
+          max_tokens: 2048,
+        }
+        await fetch('https://api.openai.com/v1/completions', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${AI_TOKEN}`
+          },
+          body: JSON.stringify(requestBody)
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.choices[0].text.trim())
+          headers.push(data.choices[0].text.trim())
+        })
+        .catch(error => console.error(error))
+      }
+      setHeaders(headers)
+    }
+    onPostAi()
+  }, [historyItems])
+
+  const formatDate = (date: Date) => {
+    // const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'][date.getDay()]
+  
+    return `${month}月${day}日 (${dayOfWeek})`
   }
 
   return (
-    <Flex direction="column" p='1rem' w='30rem'>
-      <Heading as="h2" size="md">DayBack</Heading>
-      <button id="getHistory" onClick={getHistory}>getHistory</button>
-      <button id="getCommits" onClick={onCommits}>getCommits</button>
-      <button id="getRepos" onClick={onRepos}>getRepos</button>
-      <button id="getDiaries" onClick={onDiaries}>getDiaries</button>
-      <div>
-        <ul>
-          {historyItems.map((item) => (<li id={item.id} key={item.id}>{item.title}</li>))}
-        </ul>
-      </div>
-      <div>
-        <ul>
-          {commitItems.map((item, index) => (<li id={`commit-${index}-id`} key={`commit-${index}-key`}>{item.message}</li>))}
-        </ul>
-      </div>
-      <div>
-        <ul>
-          {repoItems.map((item, index) => (<li id={`repo-${index}-id`} key={`repo-${index}-key`}>{item.name}, {item.updated_at.toISOString()}</li>))}
-        </ul>
-      </div>
-    </Flex>
+    <ContainerStyle>
+      <TitleStyle>DayBack</TitleStyle><SubTitleStyle>~わい前回何やったっけ？~</SubTitleStyle>
+      {diaries.map((diary, index) => (
+        <Accordion>
+          <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+            <Typography>
+              {formatDate(diary.date)}<br />{headers.length > index ? headers[index] : ''}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <H3Style>
+                  Github
+                </H3Style>
+                <ul>
+                  {diary.commits.map((commit, ci) => (<li id={`commit-${index}-${ci}`} key={`commit-${index}-${ci}`}>{commit.message}</li>))}
+                </ul>
+              </Grid>
+              <Grid item xs={6}>
+                <H3Style>
+                  Google
+                </H3Style>
+                <ul>
+                  {historyItems.length > index ? historyItems[index].items.map((item, hi) => (<li id={`history-${index}-${hi}`} key={`history-${index}-${hi}`}>{item.title}</li>)) : ''}
+                </ul>
+              </Grid>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </ContainerStyle>
   )
 }
+
+const ContainerStyle = styled.div`
+  width: 500px;
+`
+
+const TitleStyle = styled.h2`
+  display: inline-block;
+  font-weight: bold;
+`
+
+const SubTitleStyle = styled.h3`
+  display: inline-block;
+  margin-left: 1rem;
+  font-size: 0.5rem;
+  font-weight: none;
+`
+
+const H3Style = styled.h3`
+  font-weight: bold;
+`
 
 export default IndexPopup
